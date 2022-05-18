0c87477ae5da6208ecae8b3e8fc4ec425b85c1fd
Bruno Napoli <napolii.bruno@gmail.com>
2022-05-18 14:18:47 -0300

    corrección error y ahora imprime datos en html

--- main.js
+++ main.js
@@ -3,24 +3,22 @@ let botonInput = document.getElementById('botonBusqueda');
 function validarInput() {
     let valorInput = document.getElementById('paisBusqueda').value;
     valorInput = valorInput.toLowerCase();
+    console.log(valorInput.length)
     if (valorInput.length == 0) {
         console.log('No ha ingresado nada')
-    }
-    if (valorInput.length > 3) {
-        console.log(valorInput)
     }
     return valorInput
 };
 
-function imprimirDatos(datos) {
-    datos.forEach(element => {
-        console.log(element);
-    });
+function imprimirDatos(dato) {
+    bandera.innerHTML = `<img src="${dato[0]}" class="bandera">`
+    datos.innerHTML += `<p>Nombre país: ${dato[1]} </p>`
+    datos.innerHTML += `<p>Capital del país: ${dato[2]} </p>`
+    datos.innerHTML += `<p>Continente país: ${dato[3]} </p>`
+    datos.innerHTML += `<p>Número de población: ${dato[4]} </p>`
 };
 
 function conseguirValor(valor) {
-    // let valorInput = document.getElementById('paisBusqueda').value;
-    // valorInput = valorInput.toLowerCase();
     let datosPais = [];
     let paisURL = `https://restcountries.com/v3.1/name/${valor}?fullText=true`
     fetch (paisURL)
@@ -28,23 +26,20 @@ function conseguirValor(valor) {
         .then(data => {
             console.log(data[0])
             // bandera.innerHTML = `<img src="${data[0].flags['svg']}" class="bandera">`
+
             let bandera = data[0].flags['svg'];
-            // console.log(data[0].name['common'])
             let nombrePais = data[0].name['common'];
-            // console.log(data[0].capital[0])
             let capital = data[0].capital[0];
-            // console.log(data[0].region)
             let region = data[0].region;
-            // console.log(data[0].population)
             let reputacion = data[0].population;
-            const datosPais = [bandera, nombrePais, capital, region, reputacion]
+
+            datosPais.push(bandera, nombrePais, capital, region, reputacion)
+            imprimirDatos(datosPais);
         });
-        
-    return datosPais
 };
 
+
 botonInput.addEventListener('click', () => {
-    let valorInput = validarInput();    
-    let datos = conseguirValor(valorInput);
-    imprimirDatos(datos);
+    let valorInput = validarInput();
+    conseguirValor(valorInput);
 });
