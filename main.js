8c75cb0895bb959d63e9fa057c360381e9a21763
Bruno Napoli <napolii.bruno@gmail.com>
2022-05-18 21:08:36 -0300

    ahora se conecta a la api de clima y brinda datos acorde al pais

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let botonInput = document.getElementById('botonBusqueda');
 
+// API KEY for the weather PI = da6778deeada4cc8ab6172811221805
+
 function validarInput() {
     let valorInput = document.getElementById('paisBusqueda').value;
     valorInput = valorInput.toLowerCase();
@@ -19,18 +21,30 @@ function imprimirDatos(dato) {
 
 function conseguirValor(valor) {
     let datosPais = [];
+    let climaPais = [];
     let paisURL = `https://restcountries.com/v3.1/name/${valor}?fullText=true`
     fetch (paisURL)
         .then((response) => response.json())
         .then(data => {
             console.log(data[0])
-            // bandera.innerHTML = `<img src="${data[0].flags['svg']}" class="bandera">`
 
             let bandera = data[0].flags['svg'];
             let nombrePais = data[0].name['common'];
             let capital = data[0].capital[0];
             let region = data[0].region;
             let reputacion = data[0].population;
+
+            let climaURL = `http://api.weatherapi.com/v1/current.json?key=da6778deeada4cc8ab6172811221805&q=${capital}`;
+            fetch(climaURL)
+                .then(response => response.json())
+                .then(data => {
+                    let tempC = data.current.temp_c
+                    let tempF = data.current.temp_f
+                    let iconClima = data.current.condition.icon
+                    let descClima = data.current.condition.text
+
+                    climaPais.push(tempC, tempF, iconClima, descClima)
+                })
 
             datosPais.push(bandera, nombrePais, capital, region, reputacion)
             imprimirDatos(datosPais);
